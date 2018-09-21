33202267057b2d74e5fc1f5eec778dd296be0835
CircuitSyn <circuitsyn@gmail.com>
2018-09-21 01:22:59 -0700

    animation build

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -95,10 +95,12 @@ $(document).on('click', '.foodButton', function(){
           var titleStat = $("<p>").text('Title: ' + title);
 
           // var imgURL = response.images.fixed_height;
-          var imgURLFixed = response.data[i].images.fixed_height.url;
+          var imgURLFixed = response.data[i].images.fixed_height_still.url;
           console.log(imgURLFixed);
           var image = $('<img class="result">').attr("src", imgURLFixed);
-
+          $(image).attr("data-state", "still"); //establish still state for object
+          $(image).attr("data-still", response.data[i].images.fixed_height_still.url);
+          $(image).attr("data-animate", response.data[i].images.fixed_height.url);
           var download = '<a class="btn btn-dark btn-lg" href="' + imgURLFixed  + '" download="yourGif.gif" role="button" target="_blank"> Download Gif</a>';
           $(download).attr("href", imgURLFixed);
           console.log('img html: ' + download);
@@ -112,7 +114,20 @@ $(document).on('click', '.foodButton', function(){
     });
     });
 
-    
+    $(document).on('click', '.result', function(){
+      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+      var state = $(this).attr("data-state");
+      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+      // Then, set the image's data-state to animate
+      // Else set src to the data-still value
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+    });
 
 
 
